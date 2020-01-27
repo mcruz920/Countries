c932bb17ccac97a482e680f1f0af051757159d16
Maria <mcruz@teksystems.com>
2020-01-26 23:37:16 -0600

    Added tests

--- country-test/test/specs/country.js
+++ country-test/test/specs/country.js
@@ -1,16 +1,8 @@
-//Passing tests
 const assert = require('assert')
 
-describe('Should find the capital of the United States', () => {
-    it('Should find the capital of the United States', () => {
-        browser.url('localhost:4200')
-        $('#ex2').addValue('UsA')
-        const elem = $('#capital');
-        console.log(elem.getText());
-        assert.strictEqual(elem.getText(), 'Capital: Washington, D.C.')
-    })  
-})
+//Positive scenarios
 
+//testing country code
 describe('Should find the capital of Columbia', () => {
     it('Should find the capital of Columbia', () => {
         browser.url('localhost:4200')
@@ -20,17 +12,51 @@ describe('Should find the capital of Columbia', () => {
         assert.strictEqual(elem.getText(), 'Capital: Bogotá')
     })
 })
-
-describe('Should find the capital of Samoa', () => {
-    it('Should find the capital of Samoa', () => {
+//testing country name
+describe('Should find the capital of Australia', () => {
+    it('Should find the capital of Australia', () => {
         browser.url('localhost:4200')
-        $('#ex2').addValue('WSM')
+        $('#ex2').addValue('Australia')
         const elem = $('#capital');
         console.log(elem.getText());
-        assert.strictEqual(elem.getText(), 'Capital: Apia')
+        assert.strictEqual(elem.getText(), 'Capital: Canberra')
     })
 })
 
+//testing country code all lowercase
+describe('Should find the capital of China', () => {
+    it('Should find the capital of China', () => {
+        browser.url('localhost:4200')
+        $('#ex2').addValue('CHN')
+        const elem = $('#capital');
+        console.log(elem.getText());
+        assert.strictEqual(elem.getText(), 'Capital: Beijing')
+    })
+})
+//testing country name all lowercase
+describe('Should find the capital of French Southern Territories', () => {
+    it('Should find the capital of French Southern Territories', () => {
+        browser.url('localhost:4200')
+        $('#ex2').addValue('french southern territories')
+        const elem = $('#capital');
+        console.log(elem.getText());
+        assert.strictEqual(elem.getText(), 'Capital: Port-aux-Français')
+    })
+})
+
+//Negative scenarios
+
+//testing country name in all caps
+describe('Should find the capital of Brazil', () => {
+    it('Should find the capital of Brazil', () => {
+        browser.url('localhost:4200')
+        $('#ex2').addValue('BRAZIL')
+        const elem = $('#capital');
+        console.log(elem.getText());
+        assert.strictEqual(elem.getText(), 'Capital: Brasília')
+    })
+})
+//testing country code all caps
 describe('Should find the capital of Hungary', () => {
     it('Should find the capital of Hungary', () => {
         browser.url('localhost:4200')
@@ -40,13 +66,23 @@ describe('Should find the capital of Hungary', () => {
         assert.strictEqual(elem.getText(), 'Capital: Budapest')
     })
 })
-
-describe('Should find the capital of Australia', () => {
-    it('Should find the capital of Australia', () => {
+//testing country code with random caps
+describe('Should find the capital of the United States', () => {
+    it('Should find the capital of the United States', () => {
         browser.url('localhost:4200')
-        $('#ex2').addValue('Australia')
+        $('#ex2').addValue('UsA')
         const elem = $('#capital');
         console.log(elem.getText());
-        assert.strictEqual(elem.getText(), 'Capital: Canberra')
+        assert.strictEqual(elem.getText(), 'Capital: Washington, D.C.')
+    })  
+})
+//testing country code all caps
+describe('Should find the capital of Samoa', () => {
+    it('Should find the capital of Samoa', () => {
+        browser.url('localhost:4200')
+        $('#ex2').addValue('WSM')
+        const elem = $('#capital');
+        console.log(elem.getText());
+        assert.strictEqual(elem.getText(), 'Capital: Apia')
     })
 })
